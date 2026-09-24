7fa0bd5eeaa03fbbef481cdb30e3599b9ef2f261
fbraza <faouzi.braza@inrae.fr>
2026-09-24 19:41:07 +0200

    refactor: rename two sample to map the normal naming and add metadata to seurat object

--- R/qc/main.R
+++ R/qc/main.R
@@ -8,13 +8,14 @@ suppressPackageStartupMessages({
 
 source("R/qc/filter.R")
 source("R/qc/doublets.R")
+source("R/qc/plots.R")
 
 samples <- c(
   "REVO26-C", "REVO26-N4", "REVO26-N10", "REVO26-P4", "REVO26-P10",
   "REVO27-C", "REVO27-N4", "REVO27-N10", "REVO27-P4", "REVO27-P10",
   "REVO29-C", "REVO29-N4", "REVO29-P4", "REVO30-C", "REVO30-N4",
-  "REVO30-N10", "REVO30-P10", "REV30-P4", "REVO31-C", "REVO31-N4",
-  "REVO31-N8", "REVO31-P10", "REV31-P4"
+  "REVO30-N10", "REVO30-P10", "REVO30-P4", "REVO31-C", "REVO31-N4",
+  "REVO31-N8", "REVO31-P10", "REVO31-P4"
 )
 
 # Variable placeholder for the plot summaries
@@ -25,6 +26,23 @@ for (sample in samples) {
   # Use the function implemented before
   mex_dir <- read_filtered_matrix("raw_data", sample)
   obj <- build_seurat_obj(mex_dir, sample)
+
+  sample_parts <- strsplit(sample, "-", fixed = TRUE)[[1]]
+  condition <- sample_parts[[2]]
+  obj$sample <- sample_parts[[1]]
+  obj$pressure <- switch(
+    substr(condition, 1, 1),
+    C = "None",
+    P = "positive",
+    N = "negative",
+    stop("Unknown pressure code in sample ID: ", sample)
+  )
+  obj$time_point <- if (condition == "C") {
+    "T0H"
+  } else {
+    paste0("T", substring(condition, 2), "H")
+  }
+
   obj <- inspect_seurat_qc(obj)
 
   # collect the qc data before filtering
