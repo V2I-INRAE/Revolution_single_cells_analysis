2d7d191e9d577419f59a75dc448749859150c933
fbraza <faouzi.braza@inrae.fr>
2026-09-24 13:21:06 +0200

    qc: MT, outliers filtered for all samples

--- R/qc/main.R
+++ R/qc/main.R
@@ -8,7 +8,15 @@ suppressPackageStartupMessages({
 
 source("R/qc/filter.R")
 
-samples <- c("REVO26-C", "REVO27-C", "REVO29-C", "REVO30-C", "REVO31-C")
+samples <- c(
+  "REVO26-C", "REVO26-N4", "REVO26-N10", "REVO26-P4", "REVO26-P10",
+  "REVO27-C", "REVO27-N4", "REVO27-N10", "REVO27-P4", "REVO27-P10",
+  "REVO29-C", "REVO29-N4", "REVO29-P4",
+  "REVO30-C", "REVO30-N4", "REVO30-N10", "REVO30-P10",
+  "REV30-P4",
+  "REVO31-C", "REVO31-N4", "REVO31-N8", "REVO31-P10",
+  "REV31-P4"
+)
 for (sample in samples) {
   # Use the function implemented before
   mex_dir <- read_filtered_matrix("raw_data", sample)
